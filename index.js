60b4ace04529292d6823eea5a0ed9702a16a8d9a
alopezbelles <alopezbelles@gmail.com>
2022-10-31 18:05:32 +0100

    avances en la estructura del proyecto, constantes y puertos

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { Console } = require("console");
 const { appendFile } = require("fs");
-
+const express = require("express");
 const app = express();
+const db = require('./db/db')
+// const router = require('./router')
 
 const PORT = 3000;
 
@@ -11,11 +12,16 @@ app.use(express.json());
 
 
 
+// app.listen(PORT, () => {
+//     Console.log(`Servidor conectado en puerto ${PORT}`);
+// })
 
-
-
-
-
-app.listen(PORT, () => {
-    Console.log(`Servidor conectado en puerto ${PORT}`);
-})
+app.listen(PORT, () => {     
+    console.log(`Servidor conectado en el puerto  ${PORT}`);     
+    db.authenticate().then(()=> {         
+        console.log("Conectado a la base de datos");     
+    }).catch(error => {         
+        console.log('No se ha conectado a la base de datos' + error)     
+    })
+    
+});
