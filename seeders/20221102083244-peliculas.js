bc685efd9fe2958bb5ef857ae76190a2d90cad19
alopezbelles <alopezbelles@gmail.com>
2022-11-06 00:19:37 +0100

    seeders películas, migraciones completas

--- seeders/20221102083244-peliculas.js
+++ seeders/20221102083244-peliculas.js
@@ -2,19 +2,80 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+
+    await queryInterface.bulkInsert('peliculas', [
+
+      {
+        titulo: "El quinto elemento",
+        genero: "Ciencia ficción",
+        director: " Luc Besson",
+        valoracion: 5,
+        cine: false,
+
+      },
+
+
+      {
+        titulo: "La milla verde",
+        genero: "Drama",
+        director: "Frank Darabont",
+        valoracion: 5,
+        cine: false,
+
+      },
+
+      {
+        titulo: "Los goonies",
+        genero: "Infantil",
+        director: "Richard Donner",
+        valoracion: 4,
+        cine: false,
+
+      },
+      
+      {
+        titulo: "Hercules",
+        genero: "Infantil",
+        director: "Ron Clements",
+        valoracion: 3,
+        cine: false,
+
+      },
+      {
+        titulo: "El cuarto pasajero",
+        genero: "Terror",
+        director: "Alex de la Iglesia",
+        valoracion: 4,
+        cine: true,
+
+      },
+      {
+        titulo: "No mires a los ojos",
+        genero: "Drama",
+        director: "Félix Viscarret",
+        valoracion: 4,
+        cine: true,
+
+      },
+      {
+        titulo: "No mires a los ojos",
+        genero: "Drama",
+        director: "Félix Viscarret",
+        valoracion: 4,
+        cine: true,
+
+      },
+
+
+
+
+
+    ], {});
+
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
