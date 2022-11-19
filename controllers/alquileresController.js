4f9106ea59ad5b04468c0b860c9824a342ca8a13
alopezbelles <alopezbelles@gmail.com>
2022-11-19 18:13:39 +0100

    CRUD alquileres OK

--- controllers/alquileresController.js
+++ controllers/alquileresController.js
@@ -3,29 +3,79 @@ const AlquileresController = {};
 const models = require("../models/index");
 
 //IMPORTAMOS FUNCIONES ORM DE SEQUELIZE
-const { Op } = require("sequelize");  
+const { Op } = require("sequelize");
 
 /////  C R U D    E N D - P O I N T S  F U N C T I O N S //////
 
 //OBTENEMOS LISTADO DE LOS ALQUILERES  -------------------------------------------------
 
 AlquileresController.todosAlquileres = async (req, res) => {
-    try {
-      let resp = await models.alquileres.findAll({
-       
-      });
-      res.send(resp);
-    } catch (err) {
-      res.send(err);
-    }
-  };
+  try {
+    let resp = await models.alquileres.findAll({});
+    res.send(resp);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+//OBTENEMOS ALQUILERES POR USUARIO  -------------------------------------------------
+
+AlquileresController.alquileresPorUsuario = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let resp = await models.alquileres.findAll({
+      where: { usuarioIdUsuario: id },
+    });
+
+    res.send(resp);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+//CREAMOS UN NUEVO ALQUILER  -------------------------------------------------
+
+AlquileresController.nuevoAlquiler = async (req, res) => {
+  try {
+    let alquiler = req.body;
+    let resp = await models.alquileres.create({
+
+      fechaalquiler: alquiler.fechaalquiler,
+      fechadevolucion: alquiler.fechadevolucion,
+      articuloIdArticulos: alquiler.articuloIdArticulos,
+      usuarioIdUsuario: alquiler.usuarioIdUsuario,
+    });
+
+    res.send(resp);
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 
+//ACTUALIZAMOS UN ALQUILER EXISTENTE  -------------------------------------------------
 
+AlquileresController.actualizaAlquiler = async (req, res) => {
+  try {
+      let alquiler = req.body
+      let resp = await models.alquileres.update(
+          {
+           
+            articuloIdArticulos: alquiler.articuloIdArticulos,
+            usuarioIdUsuario: alquiler.usuarioIdUsuario,
+                        
+          },
+          {
+            where: { usuarioIdUsuario: usuarioIdUsuario }
+          }
+      )
 
+      res.send(resp)
 
-
-
+  } catch (err) {
+      res.send(err)
+  }
+}
 
 
 module.exports = AlquileresController;
--- views/alquileresRoutes.js
+++ views/alquileresRoutes.js
@@ -8,6 +8,14 @@ const AlquileresController = require("../controllers/alquileresController");
 ///// C R U D   A L Q U I L E R E S /////
 
 router.get("/", AlquileresController.todosAlquileres);
+router.get("/:id", AlquileresController.alquileresPorUsuario);
+router.post("/nuevoAlquiler", AlquileresController.nuevoAlquiler);
+router.put("/actualizaAlquiler/:id", AlquileresController.actualizaAlquiler);
+
+
+
+
+
 
 
 
